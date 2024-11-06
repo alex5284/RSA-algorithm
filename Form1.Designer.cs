8d2246f1efcd252d463cbed3d271f31ac10d27f8
ark-l <ark-l@ukr.net>
2024-11-06 22:11:31 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbp = new System.Windows.Forms.TextBox();
+            this.tbq = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRand = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.tbtext = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tbp
+            // 
+            this.tbp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbp.Location = new System.Drawing.Point(84, 23);
+            this.tbp.Name = "tbp";
+            this.tbp.Size = new System.Drawing.Size(97, 27);
+            this.tbp.TabIndex = 0;
+            // 
+            // tbq
+            // 
+            this.tbq.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbq.Location = new System.Drawing.Point(84, 62);
+            this.tbq.Name = "tbq";
+            this.tbq.Size = new System.Drawing.Size(97, 27);
+            this.tbq.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(36, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "p =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(36, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "q =";
+            // 
+            // btnRand
+            // 
+            this.btnRand.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRand.Location = new System.Drawing.Point(187, 26);
+            this.btnRand.Name = "btnRand";
+            this.btnRand.Size = new System.Drawing.Size(107, 38);
+            this.btnRand.TabIndex = 4;
+            this.btnRand.Text = "Rand";
+            this.btnRand.UseVisualStyleBackColor = true;
+            this.btnRand.Click += new System.EventHandler(this.btnRand_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalc.Location = new System.Drawing.Point(300, 23);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(152, 44);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Підписати";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // tbtext
+            // 
+            this.tbtext.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbtext.Location = new System.Drawing.Point(40, 132);
+            this.tbtext.Name = "tbtext";
+            this.tbtext.Size = new System.Drawing.Size(841, 27);
+            this.tbtext.TabIndex = 6;
+            this.tbtext.Text = "hello";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(40, 175);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(841, 104);
+            this.listBox1.TabIndex = 7;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDecrypt.Location = new System.Drawing.Point(300, 73);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(152, 47);
+            this.btnDecrypt.TabIndex = 8;
+            this.btnDecrypt.Text = "Перевірити";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(40, 285);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(841, 124);
+            this.listBox2.TabIndex = 9;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(531, 16);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(350, 104);
+            this.listBox3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 460);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tbtext);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnRand);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbq);
+            this.Controls.Add(this.tbp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbp;
+        private System.Windows.Forms.TextBox tbq;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRand;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox tbtext;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> m2;
+        int n, e, d;
+        public static bool IsPrimeNumber(uint n)
+        {
+            var result = true;
+
+            if (n > 1)
+            {
+                for (var i = 2u; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public static int ModPow(int a, int b, int m)
+        {
+            int res = 1;
+            a %= m;
+            while (b > 0)
+            {
+                if ((b & 1) != 0)
+                    res = (res * a) % m;
+                a = (a * a) % m;
+                b >>= 1;
+            }
+            if (res < 0)
+                res += m;
+            return res;
+        }
+        void Generate()
+        {
+            int n = 500;
+            List<int> x = new List<int>();
+            for (var i = 0u; i < n; i++)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    x.Add(Convert.ToInt32(i));
+                }
+            }
+            Random r = new Random();
+            int j,k = 1;
+            do
+            {
+                j = r.Next(x.Count);
+                //k = r.Next(x.Count);
+            } while (j < 3);
+            if (x[j] > 100)
+            {
+                do
+                {
+                    k = r.Next(x.Count);
+                } while (x[k] > 100);
+            }
+            else
+            {
+                do
+                {
+                    k = r.Next(x.Count);
+                } while (x[k] < 100);
+            }
+            tbp.Text = x[j].ToString();
+            tbq.Text = x[k].ToString();
+        }
+        public static bool AreRelativelyPrime(int a, int b)
+        {
+            int gcd = 1;
+            for (int i = 1; i <= a && i <= b; ++i)
+            {
+                if (a % i == 0 && b % i == 0)
+                    gcd = i;
+            }
+            return gcd == 1;
+        }
+        public static int FindD(int p, int q, int e)
+        {
+            int phi = (p - 1) * (q - 1);
+            int d = 0;
+            int k = 1;
+
+            while (true)
+            {
+                d = (k * phi + 1) / e;
+
+                if (d * e == k * phi + 1)
+                    return d;
+
+                k++;
+            }
+        }
+        void Calc()
+        {
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();
+            Random r = new Random();
+            string text = tbtext.Text;
+            byte[] hashBytes = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(text));
+            string s = Convert.ToBase64String(hashBytes);
+            listBox1.Items.Add("h1 = " + s);
+            int p = Convert.ToInt32(tbp.Text);
+            int q = Convert.ToInt32(tbq.Text);
+            n = p * q;
+            int f = (p - 1) * (q - 1);
+            List<int> e1 = new List<int>();
+            List<int> x = new List<int>();
+            bool t;
+            for (var i = 0u; i < f; i++)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    x.Add(Convert.ToInt32(i));
+                    t = AreRelativelyPrime(x[x.Count - 1], f);
+                    if (t == true) e1.Add(x[x.Count - 1]);
+                    if (e1.Count == 10) break;
+                }
+            }
+            int e2 = r.Next(0, e1.Count - 1);
+            e = e1[e2];
+            d = FindD(p, q, e);
+            m2 = Encrypt_RSA(hashBytes, d, n);
+            listBox3.Items.Add("n = " + n.ToString());
+            listBox3.Items.Add("fi = " + f.ToString());
+            listBox3.Items.Add("e = " + e.ToString());
+            listBox3.Items.Add("d = " + d.ToString());
+        }
+        List<int> Encrypt_RSA(byte[] t1, int d1, int n1)
+        {
+            byte[] text = t1;
+            List<int> c = new List<int>();
+            string c1 = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                c.Add(ModPow(text[i], d1, n1));
+                if (i != text.Length - 1) c1 += c[i].ToString() + " ";
+                else c1 += c[i].ToString();
+            }
+            
+            int l = (int)(c1.Length / 4.0);
+            string firstHalf = c1.Substring(0, l);
+            string secondHalf = c1.Substring(l, l);
+            string thirdHalf = c1.Substring(2 * l, l);
+            string forthHalf = c1.Substring(3 * l);
+            listBox1.Items.Add("h_d = " + firstHalf); 
+            listBox1.Items.Add(secondHalf);
+            listBox1.Items.Add(thirdHalf);
+            listBox1.Items.Add(forthHalf);
+
+            return c;
+        }
+
+        string Decrypt_RSA(List<int> t1, int e1, int n1)
+        {
+            byte[] m2 = new byte[t1.Count];
+            for (int i = 0; i < t1.Count; i++)
+            {
+                m2[i] = Convert.ToByte(ModPow(t1[i], e1, n1));
+            }
+            string s = Convert.ToBase64String(m2);
+            listBox2.Items.Add("h_e = " + s);
+            return s;
+        }
+
+        void Calc2()
+        {
+            listBox2.Items.Clear();
+            string text = tbtext.Text;
+            byte[] hashBytes = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(text));
+            string s = Convert.ToBase64String(hashBytes);
+            listBox2.Items.Add("h2 = " + s);
+            string s1 = Decrypt_RSA(m2, e, n);
+            if(s1 == s)
+            {
+                listBox2.Items.Add("Підпис є вірним");
+            }
+            else
+            {
+                listBox2.Items.Add("Підпис не вірним");
+            }
+        }
+        private void btnRand_Click(object sender, EventArgs e)
+        {
+            Generate();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            Calc();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            Calc2();
+        }
+    }
+}
